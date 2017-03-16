c04bd70f631430773d607e99587d7906efd3f76f
Sam Firke <samuel.firke@gmail.com>
2017-03-16 09:56:20 -0400

    fix problem with trainControl, remove redundant glm derivatives

--- scripts/train and evaluate models.R
+++ scripts/train and evaluate models.R
@@ -12,19 +12,19 @@ glm_model <- train(lower_team_wins ~ .,
                    data = past_dat,
                    method = "glm", family = "binomial")
 
-# skipping bayesglm and glmnet, they are virtually identical in result to simpler logistic regression above, correlation >.9
+# skipping bayesglm and glmnet, they are virtually identical in result to simpler logistic regression above, correlation > .9
 
-tctrl <- trainControl(method = "repeatedcv", number = 4, repeats = 3,
+tc <- trainControl(method = "repeatedcv", number = 4, repeats = 3,
                       classProbs = TRUE,
                       summaryFunction = multiClassSummary)
 
 rf_model <- train(y = train_dat$lower_team_wins,
                   x = train_dat[, -1],
                   method = "rf",
-                  metric = "logLoss",
-                  #   ntree = 151,
-                  # do.trace = TRUE,
-                  trainControl = trControl)
+                  do.trace = TRUE,
+              #    trControl = tc,
+                  ntree = 150
+                  )
 
 
 # Model evaluation
@@ -47,28 +47,33 @@ test2_ind <- createDataPartition(test_dat$lower_team_wins, list = FALSE)
 test1_dat <- test_dat[test2_ind, ]
 test2_dat <- test_dat[-test2_ind, ]
 
-preds <- data.frame(glm = predict(bayesglm_model, test1_dat, type = "prob")[, 2],
+preds <- data.frame(glm = predict(glm_model, test1_dat, type = "prob")[, 2],
                     rf = predict(rf_model, test1_dat, type = "prob")[, 2])
 
 ens1 <- train(y = test1_dat$lower_team_wins,
               x = preds,
-              method = "bayesglm", family = "binomial")
+              method = "glm", family = "binomial")
 
-ens1_preds <- predict(ens1, data.frame(glm = predict(bayesglm_model, test2_dat, type = "prob")[, 2],
+ens1_preds <- predict(ens1, data.frame(glm = predict(glm_model, test2_dat, type = "prob")[, 2],
                                        rf = predict(rf_model, test2_dat, type = "prob")[, 2],
                                        lower_team_wins = test2_dat$lower_team_wins),
                       type = "prob")[, 2]
 
 log_loss(test2_dat$lower_team_wins %>% as.numeric - 1, ens1_preds) # ensemble has 0.467 log loss, not any better
 
+
 # compare to simpler model on same data
-bayesglm_test_preds_testdat2 <- predict(bayesglm_model, test2_dat, type = "prob")[, 2]
-log_loss(test2_dat$lower_team_wins %>% as.numeric - 1, bayesglm_test_preds_testdat2)
+glm_test_preds_testdat2 <- predict(glm_model, test2_dat, type = "prob")[, 2]
 rf_test_preds_testdat2 <- predict(rf_model, test2_dat, type = "prob")[, 2]
 
-# the simpler model has log loss 0.458.  Also it is >.99 correlated with the simpler of its two feeder models, vs. .90 correlated with the random forest, implying that the RF input isn't doing much.  Original correlation between glm and rf was 0.91.
+log_loss(test2_dat$lower_team_wins %>% as.numeric - 1, glm_test_preds_testdat2)
+log_loss(test2_dat$lower_team_wins %>% as.numeric - 1, rf_test_preds_testdat2)
 
-### Make predictions ----------------------------
+# the simpler glm model has log loss 0.458.  Also it is >.99 correlated with the simpler of its two feeder models, vs. .90 correlated with the random forest, implying that the RF input isn't doing much.  Original correlation between glm and rf was 0.91.
+
+# So, skip the randomForest unless I want some more aggressive noise (see my tweet) to make a gamble on my 2nd Kaggle entry.  Maybe sample a few rows from it?
+
+### Make predictions for stage 1 ----------------------------
 
 # For stage 1:
 top_model <- glm_model
